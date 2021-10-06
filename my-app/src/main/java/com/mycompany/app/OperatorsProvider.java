ea6b8574215c369fa2d86094aaba9891395ef79a
Fatemeh Mosaddeghian <60880098+fatemeh-m@users.noreply.github.com>
2021-10-06 04:18:42 -0700

    Update OperatorsProvider.java

--- my-app/src/main/java/com/mycompany/app/OperatorsProvider.java
+++ my-app/src/main/java/com/mycompany/app/OperatorsProvider.java
@@ -4,7 +4,6 @@ import com.mycompany.Operation;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.util.HashMap;
